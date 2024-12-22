5db5c7546b155812eb76b8162e3ee0f303446b39
MrDog210 <tusakf@gmail.com>
2024-12-22 14:12:58 +0100

    SCRUM-160 dodal vektorski izris

--- core/src/main/java/com/seljaki/AgroMajsterGame/utils/Geolocation.java
+++ core/src/main/java/com/seljaki/AgroMajsterGame/utils/Geolocation.java
@@ -1,5 +1,7 @@
 package com.seljaki.AgroMajsterGame.utils;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Geolocation {
     public double lat;
     public double lng;
@@ -7,5 +9,15 @@ public class Geolocation {
     public Geolocation(double lat, double lng) {
         this.lat = lat;
         this.lng = lng;
+    }
+
+    public static float[] getPolygon(Geolocation[] loc, ZoomXY beginTile) {
+        float[] vertices = new float[loc.length*2];
+        for (int i = 0; i < loc.length; i++) {
+            Vector2 v = MapRasterTiles.getPixelPosition(loc[i].lat, loc[i].lng, beginTile.x, beginTile.y);
+            vertices[i*2] = v.x;
+            vertices[i*2 + 1] = v.y;
+        }
+        return vertices;
     }
 }
