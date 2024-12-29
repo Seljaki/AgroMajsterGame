dbb4a82666e415ab6d0b36e75b880b9d23840382
mzHap <mojcazamuda12@gmail.com>
2024-12-29 09:56:55 +0100

    Added asset manager

---
+++ core/src/main/java/com/seljaki/AgroMajsterGame/assets/RegionNames.java
@@ -0,0 +1,10 @@
+package com.seljaki.AgroMajsterGame.assets;
+
+public class RegionNames {
+
+    // gameplay.atlas
+    public static final String EXAMPLE = "example";
+
+    private RegionNames() {
+    }
+}
